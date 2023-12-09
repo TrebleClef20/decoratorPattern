018c226fb5b8b171a846458f8793e57ebc5f5d39
TrebleClef20 <65029347+TrebleClef20@users.noreply.github.com>
2023-12-09 16:47:44 +0800

    Add files via upload

---
+++ src/GSave.java
@@ -0,0 +1,45 @@
+public class GSave implements BankAccountDecorator {
+
+    private BankAccount bankAccount;
+
+    // constructor
+    public GSave(BankAccount bankAccount) {
+        setBankAccount(bankAccount);
+    }
+
+    @Override
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public String showAccountType() {
+        return "GSave";
+    }
+
+    @Override
+    public Double getInterestRate() {
+        return 0.025;
+    }
+
+    @Override
+    public Double getBalance() {
+        return this.bankAccount.getBalance();
+    }
+
+    @Override
+    public String showBenefits() {
+        return this.bankAccount.showBenefits() + ", GSave Transfer";
+    }
+
+    @Override
+    public Double computeBalanceWithInterest() {
+        Double tempBalance = this.getBalance();
+        return tempBalance + (tempBalance * this.getInterestRate());
+    }
+
+    @Override
+    public String showInfo() {
+        return this.bankAccount.showInfo();
+    }
+}
---
+++ src/UpSave.java
@@ -0,0 +1,45 @@
+public class UpSave implements BankAccountDecorator {
+
+    private BankAccount bankAccount;
+
+    // constructor
+    public UpSave(BankAccount bankAccount) {
+        setBankAccount(bankAccount);
+    }
+
+    @Override
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public String showAccountType() {
+        return "UpSave";
+    }
+
+    @Override
+    public Double getInterestRate() {
+        return 0.04;
+    }
+
+    @Override
+    public Double getBalance() {
+        return this.bankAccount.getBalance();
+    }
+
+    @Override
+    public String showBenefits() {
+        return this.bankAccount.showBenefits() + ", With Insurance";
+    }
+
+    @Override
+    public Double computeBalanceWithInterest() {
+        Double tempBalance = this.getBalance();
+        return tempBalance + (tempBalance * this.getInterestRate());
+    }
+
+    @Override
+    public String showInfo() {
+        return this.bankAccount.showInfo();
+    }
+}
